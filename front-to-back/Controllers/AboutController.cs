b75283320c9fb74f03e6c2d1e480e3aa42831842
Tamerlan9129 <tamerlankha@code.edu.az>
2022-10-05 18:15:21 +0400

    edit

--- front-to-back/Controllers/AboutController.cs
+++ front-to-back/Controllers/AboutController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using front_to_back.Models;
+using front_to_back.ViewModels.About;
+using Microsoft.AspNetCore.Mvc;
 
 namespace front_to_back.Controllers
 {
@@ -6,7 +8,20 @@ namespace front_to_back.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var chooseUs = new List<ChooseUs> {
+            new ChooseUs {  Title="Our vision",Text="Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra",FilePath="bxs-bulb" },
+            
+            
+            new ChooseUs { Title = "Our mission", Text = "Eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis.", FilePath = "bx-revision" },
+            new ChooseUs { Title = "Our goal", Text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor.", FilePath = "bxs-select-multiple" },
+
+
+
+            };
+
+            var model = new AboutIndexViewModel { ChooseUS = chooseUs };
+
+            return View(model);
         }
     }
 }
---
+++ front-to-back/Models/ChooseUs.cs
@@ -0,0 +1,12 @@
+﻿namespace front_to_back.Models
+{
+    public class ChooseUs
+    {
+        public string Title{ get; set; }
+        public string Text{ get; set; }
+        public string FilePath { get; set; }
+
+
+
+    }
+}
